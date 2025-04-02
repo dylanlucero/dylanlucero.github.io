4291d2df6102c5d48f2e41b3769b744f41e700c1
DylanLucero <dylanlucero98@gmail.com>
2025-04-01 21:42:17 -0700

    I need to stop commiting so much

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const contact = document.getElementById('contact');
+const projects = document.getElementById('projects');
+
+contact.addEventListener('click', function(){
+    window.location.replace('../contact/');
+});
+
+projects.addEventListener('click', function(){
+    window.location.replace('../projects/');
+});
+
